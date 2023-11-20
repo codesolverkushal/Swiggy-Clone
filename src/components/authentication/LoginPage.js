2c45a18e1d56b9071198c6475946e06007a8950e
kushal <mohitsinghkpsc@gmail.com>
2023-11-20 22:23:49 +0530

    Login Page Created

--- src/components/authentication/LoginPage.js
+++ src/components/authentication/LoginPage.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import { useNavigate } from 'react-router-dom';
+import './LoginPage.css'; // Import the CSS file
 
-function LoginPage() {
+const LoginPage = () => {
   const navigate = useNavigate();
 
   return (
-    <Formik
-      initialValues={{ name: '', email: '' }}
-      onSubmit={async (values) => {
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        const alertMessage = JSON.stringify(values, null, 2);
-        alert(alertMessage);
+    <div className="container">
+      <h2 className="title">Login</h2>
+      <Formik
+        initialValues={{ name: '', email: '' }}
+        onSubmit={async (values) => {
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          const alertMessage = JSON.stringify(values, null, 2);
 
-        // Use navigate function to go to "/"
-        navigate('/');
-      }}
-    >
-      <Form>
-        <Field name="name" type="text" />
-        <Field name="email" type="email" />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+      
+
+          alert(alertMessage);
+          // Use navigate function to go to "/"
+          navigate('/');
+        }}
+      >
+        <Form className="form">
+          <div className="fieldGroup">
+            <label htmlFor="name" className="label">
+              Name:
+            </label>
+            <Field name="name" type="text" className="input" />
+          </div>
+
+          <div className="fieldGroup">
+            <label htmlFor="email" className="label">
+              Email:
+            </label>
+            <Field name="email" type="email" className="input" />
+          </div>
+
+          <button type="submit" className="submitButton">
+            Submit
+          </button>
+        </Form>
+      </Formik>
+    </div>
   );
-}
+};
 
 export default LoginPage;
