10bb032d6bb0cf717763de59f70a95ac6c86c206
kushal <mohitsinghkpsc@gmail.com>
2023-12-06 23:47:56 +0530

    header styling done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { useState,useContext } from "react";
+import { useState,useContext,useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { NavLink } from "react-router-dom";
 import useOnlineStatus from './../utils/useOnlineStatus';
@@ -6,8 +6,10 @@ import  UserContext  from "../utils/UserContext"
 import { useSelector } from "react-redux";
 import { FaShoppingCart } from 'react-icons/fa';
 const Header = () => {
-  // const btnName = "Login";
+
   const [btnNameReact,setBtnNameReact] = useState("Login");
+  const [showMenu, setShowMenu] = useState(true);
+
   const onlineStatus = useOnlineStatus();
  
   const {loggedInUser} = useContext(UserContext)
@@ -16,6 +18,9 @@ const Header = () => {
   // if dependency array is empty = [] => useEffect is called on initial render(Just Once).
   // if dependency array has btnNameReact => useEffect is called everytime when btnNameReact is updated. 
 
+  const cart = useSelector((store)=>store.cart.items);
+  console.log(cart);
+
   const backgroundImageStyle = {
     backgroundImage: "url('https://images.pexels.com/photos/616412/pexels-photo-616412.jpeg?auto=compress&cs=tinysrgb&w=800')", // Placeholder image URL
     backgroundSize: "cover",
@@ -23,19 +28,42 @@ const Header = () => {
     
     // filter: "brightness(50%)", // Adjust the brightness as needed
   };
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    const closeMenuOnResize = () => {
+      if (window.innerWidth > 640) {
+        setShowMenu(false);
+      }
+    };
+
+    window.addEventListener("resize", closeMenuOnResize);
+
+    return () => {
+      window.removeEventListener("resize", closeMenuOnResize);
+    };
+  }, []);
+
   
   
-const cart = useSelector((store)=>store.cart.items);
-console.log(cart);
+
   
   return (   
-    <div className="sticky top-0 left-0 right-0 z-50 bg-green-300 shadow-lg sm:bg-yellow-200 lg:bg-green-200" style={backgroundImageStyle}>    
-    <div className="flex justify-between">
-     <div className="logo">
+    <div className="sticky top-0 left-0 right-0 z-50 bg-green-300 shadow-lg sm:bg-yellow-200 lg:bg-green-200" style={backgroundImageStyle}>   
+      <div className="flex ml-4">
+      <button className="sm:hidden font-bold text-2xl" onClick={toggleMenu}>
+            ☰
+          </button>
+     </div> 
+    <div className="flex flex-col sm:flex-row justify-between items-center">
+     <div className="logo mb-2 sm:mb-0">
           <NavLink to="/"><img className="w-28 ml-4 rounded-full" src={LOGO_URL} alt="Swiggy Logo" /></NavLink>
       </div>
       <div className="flex items-center">
-        <ul className="flex p-4 font-bold text-lg mt-5">
+        <ul className={`flex flex-col sm:flex-row p-4 font-bold text-lg mt-2 sm:mt-5 ${showMenu ? "block" : "hidden"} text-center items-center`}>
             <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
             <li className="px-4"><NavLink to="/home">Home</NavLink></li>
             <li className="px-4"><NavLink to="/about">About</NavLink></li>
@@ -47,7 +75,6 @@ console.log(cart);
 <li className="px-4 relative mr-3">
   <NavLink to="/cart" className="">
     <FaShoppingCart size={35} /> 
-    <span className="ml-1"></span>
     {cart.length > 0 && (
       <span className="bg-red-500 text-sm mx-1 text-white px-1 rounded-full absolute top-0 right-0">
         {cart.length}
