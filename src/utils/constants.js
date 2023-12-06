9ed0a3c94a728f6791fc9ecf39ac7b3d0b1f8bf8
kushal <mohitsinghkpsc@gmail.com>
2023-12-07 04:36:26 +0530

    corsproxy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,4 @@ export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,
 // export const LOGO_URL = "https://play-lh.googleusercontent.com/A8jF58KO1y2uHPBUaaHbs9zSvPHoS1FrMdrg8jooV9ftDidkOhnKNWacfPhjKae1IA";
 export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTH0Flzvbn_w9Mo14Phs0fPKhuLnKR86ZoFV4XRGAQk3R2XmCCT9NTHiVkKm12e5z5kYA&usqp=CAU";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+export const MENU_API = "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
