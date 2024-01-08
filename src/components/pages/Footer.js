58beeb3ab64435a1e1621140bce8ffcf03eccae4
KUSHAL PRATAP SINGH <130832724+codesolverkushal@users.noreply.github.com>
2024-01-08 21:35:29 +0530

    Update Footer.js

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
       };
     
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4 bottom-0">
+    <footer className="relative bg-white rounded-lg shadow dark:bg-gray-900 m-4 bottom-0">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <NavLink
